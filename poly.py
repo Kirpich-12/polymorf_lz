2c513124252ee574dcdcc1b9207758c9104682d7
Kirpich-12 <ponchik34@tuta.io>
2025-04-24 12:24:19 +0300

    go to

---
+++ poly.py
@@ -0,0 +1,15 @@
+#Deros.All rights reserved
+#=======================
+
+import pandas as pd 
+
+class Bank:
+    def __init__(self, csv):
+        self.data = csv
+
+    def anti_dubl(self):
+        df = re
+
+
+    def __del__():
+        print('This xren has been deleted')
